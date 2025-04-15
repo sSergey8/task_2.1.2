7cff4dfc56ff5d3327b262b75403b18568221533
Sergey <sevsergey8@mail.ru>
2025-04-16 01:23:58 +0300

    A ready-made task

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,24 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+
+        HelloWorld helloWorldOne =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld helloWorldTwo =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat catOne = (Cat) applicationContext.getBean("cat");
+        Cat catTwo = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(helloWorldOne.getMessage());
+        System.out.println(helloWorldTwo.getMessage());
+
+        System.out.println(catOne.getMessage());
+        System.out.println(catTwo.getMessage());
+
+        System.out.println("Hello World = " + (helloWorldOne == helloWorldTwo));
+        System.out.println("Cat = " + (catOne == catTwo));
+
+
     }
 }
